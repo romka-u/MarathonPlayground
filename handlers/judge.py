3e547550c10fb81233b6702219281921f752a866
Roman Udovichenko <mailromka@gmail.com>
2013-04-05 22:51:26 +0400

    self-remove testing script

--- handlers/judge.py
+++ handlers/judge.py
@@ -13,7 +13,8 @@ def run_judging(filename):
 			except:
 				return "Compilation error."
 				
-			with open("run_test.bat", "w") as f:
+			script_name = "run_test_{}.bat".format(datetime.datetime.now().strftime("%Y%m%dT%H%M%S"))
+			with open(script_name, "w") as f:
 				print >>f, "@echo off"
 				print >>f, "cd testing"
 				for seed in open("seeds.txt", "r"):
@@ -24,8 +25,10 @@ def run_judging(filename):
 					if seed.strip():
 						print >>f, "java -jar SnowCleaningVis.jar -novis -seed {0} -exec solution.exe > \"..\\logs\\{1}_seed_{0}.log\" 2>&1".format(seed.strip(), filename)
 
-			subprocess.Popen(["run_test.bat"])
-			return "Solution was compiled successfully, testing in process."
+				print >>f, "cd .."
+				print >>f, "del", script_name
+
+			subprocess.Popen([script_name])
 		else:
 			outfile = join("testing", "solution")
 			try:
@@ -33,7 +36,8 @@ def run_judging(filename):
 			except:
 				return "Compilation error."
 				
-			with open("run_test.sh", "w") as f:
+			script_name = "run_test_{}.sh".format(datetime.datetime.now().strftime("%Y%m%dT%H%M%S"))
+			with open(script_name, "w") as f:
 				print >>f, "cd testing"
 				for seed in open("seeds.txt", "r"):
 					if seed.strip():
@@ -43,8 +47,12 @@ def run_judging(filename):
 					if seed.strip():
 						print >>f, "java -jar SnowCleaningVis.jar -novis -seed {0} -exec './solution' > \"../logs/{1}_seed_{0}.log\" 2>&1".format(seed.strip(), filename)
 
-			subprocess.Popen(["bash", "-e", "run_test.sh"])
-			return "Solution was compiled successfully, testing in process."
+				print >>f, "cd .."
+				print >>f, "rm", script_name
+
+			subprocess.Popen(["bash", "-e", script_name])
+
+		return "Solution was compiled successfully, testing in process ({}).".format(script_name)
 			
 	except Exception, e:
 		print e
